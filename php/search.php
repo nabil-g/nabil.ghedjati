9db3cdb82c1741ed684ff431998f88e6d06c107f
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-12-16 10:09:26 +0100

    history search : date formatée en fr

--- php/search.php
+++ php/search.php
@@ -11,16 +11,16 @@
     //LECTURE DE DONNEES DANS LA BASE DE DONNEES
 
     //On prépare la requete (afin d'eviter des injections SQL).
-    $req = $bdd_cv->prepare('SELECT ID, nom, mail, message, date, time FROM messages WHERE nom LIKE ?'); // LIKE : recherche dynamique
+    $req = $bdd_cv->prepare('SELECT ID, nom, mail, message, DATE_FORMAT(date, \'%d/%m/%Y \') AS date_fr, time FROM messages WHERE nom LIKE ?'); // LIKE : recherche dynamique
 
     //On exécute la requete avec les variables "nettoyées" des éventuelles injections SQL.
     $req->execute(array($motcle));
 
-    
+
     //On utilise une boucle pr lister tous les elements de la table (je les intègre en même temps dans un tableau HTML)
 
     while ($donnees = $req->fetch()) {
-      echo "<tr><td>" . $donnees['ID']."</td><td>". $donnees['nom'] . "</td><td>" . $donnees['mail'] . "</td><td>" . $donnees['date'] ."</td><td>".$donnees['time']. "</td><td>" . $donnees['message'] . "</td></tr>" ;
+      echo "<tr><td>" . $donnees['ID']."</td><td>". $donnees['nom'] . "</td><td>" . $donnees['mail'] . "</td><td>" . $donnees['date_fr'] ."</td><td>".$donnees['time']. "</td><td>" . $donnees['message'] . "</td></tr>" ;
     }
 
 
